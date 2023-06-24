db3614e0cce1d7c8ec47a7d3e9641fa07b63d534
Luis Guilherme Lima <120605024+GuiLima22@users.noreply.github.com>
2023-06-24 01:43:01 -0300

    .

--- aula6/main.js
+++ aula6/main.js
@@ -7,13 +7,16 @@ function render(){
     
     for (const mes of ano2023.meses){
         const nomeMes = document.createElement("h2");
-        nomeMes.innerText = mes.nome
-        app.appendChild(nomeMes)
+        nomeMes.innerText = mes.nome;
+        app.appendChild(nomeMes);
         for (const lancamento of mes.lancamentos){
             const detalhesLancamento = document.createElement("p");
             detalhesLancamento.innerText = `${lancamento.tipo}  R$${lancamento.valor},00 ${lancamento.categoria}`
             nomeMes.appendChild(detalhesLancamento)
         }
+        const saldo = document.createElement("h3");
+        saldo.innerText = `Saldo: R$${mes.detalhesDoMes.saldo}`
+        app.appendChild(saldo)
         app.appendChild(document.createElement("hr"))
     }
 
