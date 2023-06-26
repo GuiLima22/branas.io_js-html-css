087f984b12b3e39a5858f62f95bc0dfe5f538af5
Luis Guilherme Lima <120605024+GuiLima22@users.noreply.github.com>
2023-06-25 21:59:08 -0300

    .

--- aula7/classes.js
+++ aula7/classes.js
@@ -12,7 +12,7 @@ class Lancamento {
         this.tipo = tipo;
         this.valor = valor;
         this.categoria = categoria;
-    };
+    }
 }
 
 
@@ -24,11 +24,11 @@ class Mes {
         this.nome = nome;
         this.saldoInicial = saldoInicial;
         this.lancamentos = [];
-    };
+    }
 
     adicionarLancamentos(lancamento) {
         this.lancamentos.push(lancamento);
-    };
+    }
 
     calcularSaldo() {
         this.detalhesDoMes.saldo += this.saldoInicial;
@@ -36,46 +36,46 @@ class Mes {
             if (lancamento.tipo === "receita") {
                 this.detalhesDoMes.saldo += arredondar(lancamento.valor);
                 this.detalhesDoMes.receitas += arredondar(lancamento.valor);
-            };
+            }
             if (lancamento.tipo === "despesa") {
                 this.detalhesDoMes.saldo -= arredondar(lancamento.valor);
                 this.detalhesDoMes.despesas += arredondar(lancamento.valor);
-            };
-        };
-    };
+            }
+        }
+    }
 
     calcularPercentualDespesas() {
         for (const lancamento of this.lancamentos) {
             if (lancamento.tipo === "despesa")
                 this.detalhesDoMes.percentualDespesas.push({ percentual: arredondar(lancamento.valor / this.detalhesDoMes.despesas * 100), categoria: lancamento.categoria })
         }
-    };
+    }
 
     calcularJuros() {
         this.detalhesDoMes.juros = arredondar(this.detalhesDoMes.saldo * 0.1)
         this.detalhesDoMes.saldo -= this.detalhesDoMes.juros
-    };
+    }
 
     calcularRendimentos() {
         this.detalhesDoMes.rendimentos = arredondar(this.detalhesDoMes.saldo * 0.005)
         this.detalhesDoMes.saldo += this.detalhesDoMes.rendimentos
-    };
+    }
 
     definirJurosOuRendimento() {
         if (this.detalhesDoMes.saldo < 0) {
             this.calcularJuros();
-        };
+        }
         if (this.detalhesDoMes.saldo > 0) {
             this.calcularRendimentos();
-        };
-    };
+        }
+    }
 
     calcularFinancas() {
         this.detalhesDoMes = { saldo: 0, juros: 0, rendimentos: 0, receitas: 0, despesas: 0, percentualDespesas: [] };
         this.calcularSaldo();
         this.calcularPercentualDespesas();
         this.definirJurosOuRendimento();
-    };
+    }
 
 }
 
@@ -87,7 +87,7 @@ class Ano {
     }
 
     adicionarMes(mes) {
-        this.meses.push(mes);
+        this.meses.push(mes)
     }
 
     adicionarLancamento(nomeMes, lancamento){
--- aula7/main.js
+++ aula7/main.js
@@ -1,56 +1,56 @@
-function addDocElements (element, text, ref){
+function addDocElements(element, text, ref) {
     const constant = document.createElement(element);
     constant.innerText = text;
     ref.appendChild(constant);
 }
 
-function render(){
+function render() {
     const app = document.getElementById("app");
-    if (app.firstChild){
+    if (app.firstChild) {
         app.firstChild.remove();
     }
 
     const panel = document.createElement("div");
-    
-    for (const mes of ano2023.meses){
+
+    for (const mes of ano2023.meses) {
         addDocElements("h2", mes.nome, panel);
 
         const tableCreation = document.createElement("table");
-        tableCreation.className = "tableStyle"
+        tableCreation.className = "tableStyle";
 
         const lineTitle = document.createElement("tr");
         addDocElements("th", "categoria", lineTitle);
         addDocElements("th", "valor", lineTitle);
         tableCreation.appendChild(lineTitle);
 
-        for (const lancamento of mes.lancamentos){
+        for (const lancamento of mes.lancamentos) {
             const lineCreation = document.createElement("tr");
             addDocElements("td", lancamento.categoria, lineCreation);
-            addDocElements("td", lancamento.valor, lineCreation);
+            addDocElements("td", moneyFormat(lancamento.valor), lineCreation);
             tableCreation.appendChild(lineCreation);
             panel.appendChild(tableCreation);
 
-            if (lancamento.tipo == 'despesa'){
-                lineCreation.className = "despesa"
+            if (lancamento.tipo == 'despesa') {
+                lineCreation.className = "despesa";
             }
-            else if (lancamento.tipo == 'receita'){
-                lineCreation.className = "receita"
+            else if (lancamento.tipo == 'receita') {
+                lineCreation.className = "receita";
             }
         }
 
         const lineRendimentos = document.createElement("tr");
         addDocElements("th", "rendimentos", lineRendimentos);
-        addDocElements("th", mes.detalhesDoMes.rendimentos, lineRendimentos);
+        addDocElements("th", moneyFormat(mes.detalhesDoMes.rendimentos), lineRendimentos);
         tableCreation.appendChild(lineRendimentos);
 
         const lineJuros = document.createElement("tr");
         addDocElements("th", "juros", lineJuros);
-        addDocElements("th", mes.detalhesDoMes.juros, lineJuros);
+        addDocElements("th", moneyFormat(mes.detalhesDoMes.juros), lineJuros);
         tableCreation.appendChild(lineJuros);
 
         const lineSaldo = document.createElement("tr");
         addDocElements("th", "total", lineSaldo);
-        addDocElements("th", mes.detalhesDoMes.saldo, lineSaldo);
+        addDocElements("th", moneyFormat(mes.detalhesDoMes.saldo), lineSaldo);
         tableCreation.appendChild(lineSaldo);
     }
 
@@ -59,19 +59,19 @@ function render(){
 
 render();
 
-function addLancamento(){
+function addLancamento() {
     const month = document.getElementById("mes");
     const type = document.getElementById("tipo");
     const value = document.getElementById("valor");
     const category = document.getElementById("categoria");
-    ano2023.adicionarLancamento(month.value, new Lancamento(type.value, parseFloat(value.value), category.value))
+    ano2023.adicionarLancamento(month.value, new Lancamento(type.value, parseFloat(value.value), category.value));
     ano2023.calcularSaldo();
     render();
     month.value = "";
     type.value = "";
     value.value = "";
     category.value = "";
-    console.log(month.value)
+    console.log(month.value);
 }
 
 const botao = document.getElementById("botao");
@@ -79,8 +79,8 @@ botao.addEventListener("click", addLancamento);
 
 const mesSelect = document.getElementById("mes");
 
-for(mes of ano2023.meses){
+for (mes of ano2023.meses) {
     const option = document.createElement("option");
-    option.text = mes.nome
-    mesSelect.appendChild(option)
+    option.text = mes.nome;
+    mesSelect.appendChild(option);
 }
